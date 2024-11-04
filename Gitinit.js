67064b0013a978cc6893dd29c7229c2e2a5188e6
EloisaJosue <eloisajoyjosue@gmail.com>
2024-11-04 18:46:12 +0800

    Create Gitinit.js

---
+++ Gitinit.js
@@ -0,0 +1,15 @@
+let age = "20";
+
+if (age < 5) {
+    console.log("You are a toddler.");
+} else if (age <= 10) {
+    console.log("You are a child.");
+} else if (age <= 18) {
+    console.log("You are a teenager.");
+} else if (age <= 30) {
+    console.log("You are a young adult.");
+} else if (age <= 60) {
+    console.log("You are middle-aged.");
+} else {
+    console.log("You are a senior.")
+}
